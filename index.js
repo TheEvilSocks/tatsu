1ca4ffd86242b776f45fecd9da0e7ca04a66138c
TheEvilSocks <TheEvilSocks@gmail.com>
2021-04-03 13:49:24 +0200

    Add some jsdoc
    Signed-off-by: TheEvilSocks <TheEvilSocks@gmail.com>

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const TatsuClient = require('./lib/Tatsu.js');
 
+/**
+* Create a new API client
+* @arg {string} apiKey Your Tatsu API key, request one by using `t!apikey` on Discord.
+* @arg {Object} [options] Optional overrides for certain library settings.
+* @arg {Object} [options] Optional overrides for certain library settings.
+* @returns TatsuClient
+*/
 function Tatsu(apiKey, options) {
 	return new TatsuClient(apiKey, options);
 }
